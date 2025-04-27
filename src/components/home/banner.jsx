ee1409ebd1668ddcbcc73fb7c111a8b7ffb68747
Polo-commits <routeitsolutions@gmail.com>
2025-04-27 16:11:43 +0000

    Fix folder structure and sanity client import

--- src/components/home/banner.jsx
+++ src/components/home/banner.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import { Link } from "react-router-dom";
-import sanityClient from "../../sanityClient";
+import sanityClient from "../../sanity/sanityClient";
 
 export default function Banner() {
   const [data, setData] = useState(null);
