d4f29252d57e046fd55dbf1db0449594da60c690
Polo-commits <routeitsolutions@gmail.com>
2025-04-27 13:15:24 +0000

    Fix sanityClient import path

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import sanityClient from '../sanityClient';
+import sanityClient from "../../sanityClient";
 
 export default function Gallery() {
   const [galleryItems, setGalleryItems] = useState([]);
