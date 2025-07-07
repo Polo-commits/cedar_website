ad71551415d11a0996a0e7f926f089d3a223d8b2
Polo-commits <routeitsolutions@gmail.com>
2025-07-07 05:43:15 +0000

    Add services schema and update configs

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,9 +1,11 @@
 import galleryItem from './galleryItem'
-
+import services from './services';
 import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
 import siteSettings from './siteSettings'
 
-export const schemaTypes = [post, author, category, blockContent, siteSettings, galleryItem]
+import homepage from './homepage'
+
+export const schemaTypes = [post, author, category, blockContent, siteSettings, galleryItem, services, homepage]
---
+++ schemaTypes/services.js
@@ -0,0 +1,33 @@
+export default {
+  name: 'services',
+  title: 'Services',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Service Title',
+      type: 'string',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'bulletPoints',
+      title: 'Bullet Points',
+      type: 'array',
+      of: [{ type: 'string' }],
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
